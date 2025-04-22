4b4d5930ad207b4eb204c02ca6e9de8efb9fb555
sudharshanreddy-dev <sudharshanreddy.dev@gmail.com>
2025-04-22 21:35:28 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,35 +1,3 @@
-// import express from "express";
-// import dotenv from "dotenv";
-// import { auth_router } from "./route/auth.route.js";
-// import cookieParser from "cookie-parser";
-
-// //configurations
-// dotenv.config();
-
-
-// const app = express();
-
-// //middleware
-// app.use(cookieParser())
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json())
-
-
-
-
-// app.get('/test', (req, res) => {
-//     res.send('test route wworking');
-// })
-
-// app.use('/auth', auth_router);
-
-
-
-// app.listen(5000, () => {
-//     console.log('server is running on port 5000');
-// })
-
-
 import { httpServer } from "./app.js";
 import "./utils/socket.js";
 
